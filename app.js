71681fb25adfcf7aff76ce6eb2d0251557ac2fc6
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2022-02-21 19:01:54 +0200

    tehtävään 4.2 asti

---
+++ app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')    
+const mongoose = require('mongoose')
+
+
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+    .then(result => {
+        logger.info('Connected to mongoDB')
+    })
+    .catch((error) => {
+        logger.error('Error connecting to MongoDB', error.message)
+})
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
